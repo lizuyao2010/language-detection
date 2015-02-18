4498dc005d6b57a44ed780348743ce4ecb6bb192
lizuyao2010 <zuyaoli2010@gmail.com>
2015-02-18 13:11:55 -0800

    change dir

--- src/language_detector.py
+++ src/language_detector.py
@@ -7,7 +7,6 @@ from collections import defaultdict
 def get_langlist(root):
     langlist=[]
     for filename in os.listdir(root):
-        #if filename in ['en','fr','de','zh-CN']:
         with open(root+'/'+filename) as fp:
             lan=json.load(fp)
             langlist.append(lan)
@@ -26,14 +25,13 @@ def get_features(text):
         grams.append(g)
     return grams
 
-
-
 def predict(langlist,text,order):
     grams=get_features(text)
     pre_label=''
     pre_score=float('-infinity')
     
     v=sum(language['n_words'][order-1] for language in langlist)
+    predict_list=[]
     for language in langlist:
         total_score=0.0
         for feature in grams[order-1]:
@@ -41,17 +39,17 @@ def predict(langlist,text,order):
                 total_score+=math.log(1.0/v)
             else:
                 total_score+=math.log(language['freq'][feature]/float(language['n_words'][order-1]))
-        if total_score!=0 and (pre_label=='' or total_score>pre_score):
-            pre_label=language['name']
-            pre_score=total_score
-            print pre_label,pre_score
-    return pre_label
+        predict_list.append((total_score,language['name']))
+    pre_label=max(item for item in predict_list)
+    #print sorted(predict_list,reverse=True)[:5]
+    return pre_label[1]
 
 
 if __name__=='__main__':
-    root='../languages'
+    root="languages"
+    order=2
     langlist=get_langlist(root)
     with open(sys.argv[1],'r') as fr:
         text=fr.read().strip().decode('UTF-8')
-    pre_label=predict(langlist,text,2)  
-    print pre_label
+    pre_label=predict(langlist,text,order)  
+    print "predicted language type is:", pre_label
